f24b813b2d903dcda2be7d44029f2ead67de92f3
Beichen <lbc0430@gmail.com>
2017-02-22 15:01:55 -0500

    add calcsize

---
+++ calcSize.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import urllib2
+import sys
+
+urls = []
+Sum = 0
+def getUrl(inf):
+    global urls
+    with open(inf) as f:
+         lines = f.readlines()
+
+    for line in lines:
+        urls.append( line.split(' ')[2])
+    calcSize(urls)
+
+def calcSize(urls):
+    global Sum
+    for url in urls:
+        U = urllib2.urlopen(url)
+        size = len(U.read())
+    Sum+= size
+
+def main():
+    getUrl(sys.argv[1])
+
+
+main()
+
+print Sum, len(urls)
