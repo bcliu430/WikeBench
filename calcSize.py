c613fac4b352c02e80f3dab7a9a1f737e60c614f
root <lbc0430@gmail.com>
2017-02-25 15:48:05 -0500

    add download and unzip script

--- calcSize.py
+++ calcSize.py
@@ -5,6 +5,7 @@ import collections
 #import numpy as np
 #import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
+import json
 
 urls = []
 sizelist = []
@@ -20,24 +21,29 @@ def getUrl(inf):
 
 def calcSize(urls):
     global sortdict
-    length = len(urls)/360000
+    fout = open(sys.argv[1]+'.out','w')
+    out = sys.argv[1] + '.pdf'
+    length = len(urls)/3600  # 1hour /3600 = 1sec
     for i in range(length):
         try:
             U = urllib2.urlopen(urls[i])
         except:
             continue
         size = len(U.read())
-        sizelist.append(size)
-        sortdict = {x:sizelist.count(x) for x in sizelist}     
-    plt.bar( range(len(sortdict)), sortdict.values(), align='center')
-    plt.xticks(range(len(sortdict)),sortdict.keys())
+        if( size <= 100000):
+            sizelist.append(size)
+    sortdict = {x:sizelist.count(x) for x in sizelist}
+    print sortdict
+    json.dump(sortdict,fout)    
+    fout.close()     
+    plt.hist(sizelist,bins=10)
     plt.xlabel('size of wikipedia')
     plt.ylabel('occurance')
     plt.title('Wikipedia Tracefile')
-    plt.grid(True)
-    plt.savefig('result.pdf')    
+    plt.savefig(out)    
 
 def main():
     getUrl(sys.argv[1])
 
 main()
+
