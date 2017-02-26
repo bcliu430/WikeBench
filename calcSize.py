6dfb487cb64edea9ea317b7b7941e21b260eff1d
root <lbc0430@gmail.com>
2017-02-26 01:05:28 -0500

    add calc.py

--- calcSize.py
+++ calcSize.py
@@ -2,48 +2,54 @@
 import urllib2
 import sys
 import collections
-#import numpy as np
-#import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import json
+import os
 
 urls = []
 sizelist = []
 sortdict = {}
-def getUrl(inf):
-    global urls
-    with open(inf) as f:
-         lines = f.readlines()
+dire = "./trace/"
+Files = os.listdir(dire)
 
-    for line in lines:
-        urls.append( line.split(' ')[2])
+def getUrl():
+ #   f = open('stats','w')
+    global urls
+    for inf in Files: 
+        with open(dire+inf) as f:
+            lines = f.readlines()
+        for line in lines:
+            urls.append( line.split(' ')[2])
     calcSize(urls)
 
 def calcSize(urls):
     global sortdict
-    fout = open(sys.argv[1]+'.out','w')
-    out = sys.argv[1] + '.pdf'
-    length = len(urls)/3600  # 1hour /3600 = 1sec
+    valid_url = 0
+    urlLessThan50k = 0
+#    fout = open('stats','w')
+    length = len(urls)/7200  # 1hour /7200 = 0.5sec
     for i in range(length):
         try:
             U = urllib2.urlopen(urls[i])
+            valid_url += 1
         except:
             continue
         size = len(U.read())
-        if( size <= 100000):
+        if( size <= 50000):
+            urlLessThan50k += 1 
             sizelist.append(size)
-    sortdict = {x:sizelist.count(x) for x in sizelist}
-    print sortdict
-    json.dump(sortdict,fout)    
+    fout = open('stats','w')
+    out = "urls: " + str(length) + " valid: " + str(valid_url) + " url < 50k: " + str(urlLessThan50k)
+    fout.write(out)
     fout.close()     
-    plt.hist(sizelist,bins=10)
+    plt.hist(sizelist,bins=20)
     plt.xlabel('size of wikipedia')
     plt.ylabel('occurance')
     plt.title('Wikipedia Tracefile')
-    plt.savefig(out)    
+    plt.savefig('plot.png')    
 
 def main():
-    getUrl(sys.argv[1])
+    getUrl()
 
 main()
 
