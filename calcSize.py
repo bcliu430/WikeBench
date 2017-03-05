b2d068dff1c0a38c2b68d9ad58b1400c3624f996
Beichen <lbc0430@gmail.com>
2017-03-05 15:08:29 -0500

    modify files

--- calcSize.py
+++ calcSize.py
@@ -13,20 +13,18 @@ dire = "./trace/"
 Files = os.listdir(dire)
 
 def getUrl():
- #   f = open('stats','w')
     global urls
     for inf in Files: 
         with open(dire+inf) as f:
             lines = f.readlines()
         for line in lines:
             urls.append( line.split(' ')[2])
-    calcSize(urls)
+    saveSize(urls)
 
-def calcSize(urls):
+def saveSize(urls):
     global sortdict
     valid_url = 0
     urlLessThan50k = 0
-#    fout = open('stats','w')
     length = len(urls)/7200  # 1hour /7200 = 0.5sec
     for i in range(length):
         try:
@@ -36,17 +34,16 @@ def calcSize(urls):
             continue
         size = len(U.read())
         if( size <= 50000):
-            urlLessThan50k += 1 
-            sizelist.append(size)
-    fout = open('stats','w')
-    out = "urls: " + str(length) + " valid: " + str(valid_url) + " url < 50k: " + str(urlLessThan50k)
-    fout.write(out)
-    fout.close()     
+	    with open ("data.out","a") as output:
+	    	output.write(size)
+'''		
+    
     plt.hist(sizelist,bins=20)
     plt.xlabel('size of wikipedia')
     plt.ylabel('occurance')
     plt.title('Wikipedia Tracefile')
     plt.savefig('plot.png')    
+'''
 
 def main():
     getUrl()
